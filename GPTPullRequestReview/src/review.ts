a9d5bce64c8184d107465a12e8e89a8450f3b2e7
Gabriel Ecegi <gabriel.ecegi@blogic.cz>
2023-09-30 11:41:32 +0200

    Add surrounding code feature

--- GPTPullRequestReview/src/review.ts
+++ GPTPullRequestReview/src/review.ts
@@ -4,6 +4,88 @@ import { CreateChatCompletionResponseChoicesInner, OpenAIApi } from "openai";
 import { addCommentToPR } from "./pr";
 import { Agent } from "https";
 import * as tl from "azure-pipelines-task-lib/task";
+import { encoding_for_model, TiktokenModel } from "@dqbd/tiktoken";
+
+const MAX_TOKENS = 2048; // This is an example. Adjust based on your OpenAI plan.
+
+function countTokens(model: string, str: string): number {
+  try {
+    const encoder = encoding_for_model(model as TiktokenModel);
+
+    const tokens = encoder.encode(str);
+    encoder.free();
+    return tokens.length;
+  } catch (error: any) {
+    return 0;
+  }
+}
+
+function truncateContent(content: string, maxTokens: number): string {
+  const tokens = content.split(/\s+/);
+  return tokens.slice(0, maxTokens).join(" ");
+}
+
+async function isFileWithIgnoredGitStatus(fileName: string) {
+  const fileStatus = await git.status([fileName]);
+
+  return fileStatus.deleted.length > 0;
+}
+
+const IGNORED_EXTENSIONS = [
+  ".png",
+  ".jpg",
+  ".jpeg",
+  ".gif",
+  ".bmp",
+  ".tiff",
+  ".svg",
+  ".ico",
+  ".csv",
+  ".json",
+  ".zip",
+  ".tar",
+  ".gz",
+  ".rar",
+  ".pdf",
+  ".doc",
+  ".docx",
+  ".xls",
+  ".xlsx",
+  ".ppt",
+  ".pptx",
+  ".mp3",
+  ".mp4",
+  ".avi",
+  ".mov",
+  ".webm",
+  ".ogg",
+  ".wav",
+];
+
+function isBinary(content: string): boolean {
+  // Check for non-textual characters in the first few bytes of the content
+  for (let i = 0; i < Math.min(24, content.length); i++) {
+    const charCode = content.charCodeAt(i);
+    if (charCode === 0 || charCode > 127) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function isFileWithIgnoredFileExtension(
+  fileName: string,
+  content: string
+): boolean {
+  const fileExtension = fileName.slice(
+    ((fileName.lastIndexOf(".") - 1) >>> 0) + 2
+  );
+
+  if (IGNORED_EXTENSIONS.includes(`.${fileExtension}`) || isBinary(content)) {
+    return true;
+  }
+  return false;
+}
 
 export async function reviewFile(
   targetBranch: string,
@@ -15,27 +97,84 @@ export async function reviewFile(
 ) {
   console.log(`Start reviewing ${fileName} ...`);
 
+  // const fileStatus = await git.status([fileName]);
+  const isIgnoredGitStatus = await isFileWithIgnoredGitStatus(fileName);
+  if (isIgnoredGitStatus) {
+    console.log(`${fileName} is deleted. Skipping review.`);
+    return;
+  }
+
+  let fileContent = await git.show([`${targetBranch}:${fileName}`]);
+
+  const isIgnoredFileExtension = isFileWithIgnoredFileExtension(
+    fileName,
+    fileContent
+  );
+
+  if (isIgnoredFileExtension) {
+    console.log(`${fileName} is ignored. Skipping review.`);
+    return;
+  }
+
   const defaultOpenAIModel = "gpt-3.5-turbo";
   const patch = await git.diff([targetBranch, "--", fileName]);
 
-  const noFeedback = "#NF";
+  const noFeedback = "NF";
 
-  const instructions = `
-Review the provided Pull Request changes in patch format. Each patch entry has the commit message in the Subject line followed by the code changes (diffs) in a unidiff format.
+  let instructions = `
+Review PR changes provided in unidiff format. Focus on the content, not formatting.
 Tasks:
-- Review only added, edited, or deleted lines. Ignore formatting changes.
-- As the first sentence, rate the severity of each issue (1-5, with 5 being the most severe) and optionally add an emoji. Format: 'Severity: 3 :emoji:'
-- Focus on issues. If all is good, simply write '${noFeedback}' Otherwise, provide concise feedback and end with a compliment if deserved.
-Another rules:
-- Always prefer 'if (!!object)' over 'if (object)'.
+- If no issues, respond with '${noFeedback}'.
+- Rate issues (1-5, 5 highest). Optionally, add an emoji: 'Severity: 3 :emoji:'.
+- Provide concise feedback. Offer fixes if possible.
+- Check for:
+  1. Code Consistency: Align with established conventions and patterns.
+  2. Performance: Identify inefficiencies or bottlenecks.
+  3. Security: Spot vulnerabilities or risky practices.
+  4. Readability: Ensure code is clear with meaningful names and comments.
+  5. Error Handling: Check for graceful error or exception handling.
+  6. Compatibility: Ensure changes maintain backward compatibility or are flagged if not.
+  7. Best Practices: Adhere to industry and language-specific standards.
+- Be precise; senior devs will review your feedback.
+- Keep comments brief. Developers will investigate further if needed.
+Rules:
+1. Prioritize 'if (!!object)' over 'if (object)'.
+2. Use 'const' for variables that won't be reassigned.
+3. Use early returns to avoid nested 'if' statements.
+4. Use descriptive names. Avoid abbreviations.
+5. Avoid magic numbers; use named constants.
+6. Keep functions/methods short and focused on a single task.
+7. Use comments sparingly. Prefer self-explanatory code.
 `;
+
+  const customPrompt = tl.getInput("custom_prompt");
+  if (!!customPrompt) {
+    if (tl.getBoolInput("override_prompt")) {
+      instructions = customPrompt;
+    } else {
+      instructions = `${customPrompt}\n${instructions}`;
+    }
+  }
+  const model = tl.getInput("model") || defaultOpenAIModel;
+
+  const totalTokens = countTokens(model, instructions + patch + fileContent);
+
+  // This is just the first version, not sure about the best way to handle this.
+  if (totalTokens > MAX_TOKENS) {
+    console.warn(
+      `Content exceeds token limit by ${
+        totalTokens - MAX_TOKENS
+      } tokens. Truncating...`
+    );
+    fileContent = truncateContent(fileContent, MAX_TOKENS - 100); // Reserve some tokens for potential API overhead.
+  }
 
   try {
     let choices: CreateChatCompletionResponseChoicesInner[] = [];
 
     if (openai) {
       const response = await openai.createChatCompletion({
-        model: tl.getInput("model") || defaultOpenAIModel,
+        model: model,
         messages: [
           {
             role: "system",
@@ -44,6 +183,10 @@ Another rules:
           {
             role: "user",
             content: patch,
+          },
+          {
+            role: "user",
+            content: `Surrounding code : ${fileContent}`,
           },
         ],
         max_tokens: 500,
@@ -72,8 +215,9 @@ Another rules:
 
     if (choices && choices.length > 0) {
       const review = choices[0].message?.content as string;
+      console.log(review);
 
-      if (review.trim() !== noFeedback) {
+      if (!review.trim().startsWith(noFeedback)) {
         await addCommentToPR(fileName, review, httpsAgent);
       }
     }
