adbeec97f8643455b8c46d10fd09d8b3a8930974
Gabriel Ecegi <gabriel.ecegi@blogic.cz>
2023-09-30 07:51:19 +0200

    Improve prompt

--- GPTPullRequestReview/src/review.ts
+++ GPTPullRequestReview/src/review.ts
@@ -1,57 +1,68 @@
-import fetch from 'node-fetch';
-import { git } from './git';
-import { OpenAIApi } from 'openai';
-import { addCommentToPR } from './pr';
-import { Agent } from 'https';
+import fetch from "node-fetch";
+import { git } from "./git";
+import { OpenAIApi } from "openai";
+import { addCommentToPR } from "./pr";
+import { Agent } from "https";
 import * as tl from "azure-pipelines-task-lib/task";
 
-export async function reviewFile(targetBranch: string, fileName: string, httpsAgent: Agent, apiKey: string, openai: OpenAIApi | undefined, aoiEndpoint: string | undefined) {
+export async function reviewFile(
+  targetBranch: string,
+  fileName: string,
+  httpsAgent: Agent,
+  apiKey: string,
+  openai: OpenAIApi | undefined,
+  aoiEndpoint: string | undefined
+) {
   console.log(`Start reviewing ${fileName} ...`);
 
-  const defaultOpenAIModel = 'gpt-3.5-turbo';
-  const patch = await git.diff([targetBranch, '--', fileName]);
+  const defaultOpenAIModel = "gpt-3.5-turbo";
+  const patch = await git.diff([targetBranch, "--", fileName]);
 
-  const instructions = `Act as a code reviewer of a Pull Request, providing feedback on possible bugs and clean code issues.
-        You are provided with the Pull Request changes in a patch format.
-        Each patch entry has the commit message in the Subject line followed by the code changes (diffs) in a unidiff format.
+  const noFeedback = "#NF";
 
-        As a code reviewer, your task is:
-                - Review only added, edited or deleted lines.
-                - If there's no bugs and the changes are correct, write only 'No feedback.'
-                - If there's bug or uncorrect code changes, don't write 'No feedback.'`;
+  const instructions = `
+Review the provided Pull Request changes in patch format. Each patch entry has the commit message in the Subject line followed by the code changes (diffs) in a unidiff format.
+Tasks:
+- Review only added, edited, or deleted lines. Ignore formatting changes.
+- As the first sentence, rate the severity of each issue (1-5, with 5 being the most severe) and optionally add an emoji. Format: 'Severity: 3 :emoji:'
+- Focus on issues. If all is good, simply write '${noFeedback}' Otherwise, provide concise feedback and end with a compliment if deserved.
+Another rules:
+- Always prefer 'if (!!object)' over 'if (object)'.
+`;
 
   try {
     let choices: any;
 
     if (openai) {
       const response = await openai.createChatCompletion({
-        model: tl.getInput('model') || defaultOpenAIModel,
+        model: tl.getInput("model") || defaultOpenAIModel,
         messages: [
           {
             role: "system",
-            content: instructions
+            content: instructions,
           },
           {
             role: "user",
-            content: patch
-          }
+            content: patch,
+          },
         ],
-        max_tokens: 500
+        max_tokens: 500,
       });
 
-      choices = response.data.choices
-    }
-    else if (aoiEndpoint) {
+      choices = response.data.choices;
+    } else if (aoiEndpoint) {
       const request = await fetch(aoiEndpoint, {
-        method: 'POST',
-        headers: { 'api-key': `${apiKey}`, 'Content-Type': 'application/json' },
+        method: "POST",
+        headers: { "api-key": `${apiKey}`, "Content-Type": "application/json" },
         body: JSON.stringify({
           max_tokens: 500,
-          messages: [{
-            role: "user",
-            content: `${instructions}\n, patch : ${patch}}`
-          }]
-        })
+          messages: [
+            {
+              role: "user",
+              content: `${instructions}\n, patch : ${patch}}`,
+            },
+          ],
+        }),
       });
 
       const response = await request.json();
@@ -62,14 +73,13 @@ export async function reviewFile(targetBranch: string, fileName: string, httpsAg
     if (choices && choices.length > 0) {
       const review = choices[0].message?.content as string;
 
-      if (review.trim() !== "No feedback.") {
+      if (review.trim() !== noFeedback) {
         await addCommentToPR(fileName, review, httpsAgent);
       }
     }
 
     console.log(`Review of ${fileName} completed.`);
-  }
-  catch (error: any) {
+  } catch (error: any) {
     if (error.response) {
       console.log(error.response.status);
       console.log(error.response.data);
